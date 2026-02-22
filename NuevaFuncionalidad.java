b81209250353d032fb936ef3e42b9d37d2011d55
saracasta52-star <saracasta52@gmail.com>
2026-02-22 19:09:07 +0100

    Create NuevaFuncionalidad.java
    Añadir NuevaFuncionalidad.java.

---
+++ NuevaFuncionalidad.java
@@ -0,0 +1,12 @@
+package desarrollo;
+
+public class NuevaFuncionalidad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Gestionando repositorios!");
+
+	}
+
+}
